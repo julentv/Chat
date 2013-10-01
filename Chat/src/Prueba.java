924f47a6e5c8f59cd94f30798e5759461a61302d
Julen Windows <julentv@gmail.com>
2013-10-01 17:19:02 +0200

    ads

--- Chat/src/Prueba.java
+++ Chat/src/Prueba.java
@@ -1,6 +1,6 @@
 
 public class Prueba {
 	public static void main (String [] args){
-		System.out.println("Julen es pesado al cuadrado");
+		System.out.println("Raquel es pesado al cuadrado");
 	}
 }
